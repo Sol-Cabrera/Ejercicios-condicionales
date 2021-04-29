5a2dca0204772cec517257dd7c5437194abf9a1d
Sol-Cabrera <80895344+Sol-Cabrera@users.noreply.github.com>
2021-04-29 09:28:03 -0300

    Add files via upload

---
+++ ActividadDesarrollo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividaddesarrollo;
+
+/**
+ *
+ * @author pc13
+ */
+public class ActividadDesarrollo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int a=1;
+    for(a=0;a<=100;a++) {
+    if(a%2==0 && a%3==0)
+            System.out.println(a);
+    }
+    a++;
+    
+    
+    
+    
+    }
+    
+}
---
+++ ActividadDesarrollo4.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividaddesarrollo4;
+
+/**
+ *
+ * @author pc13
+ */
+public class ActividadDesarrollo4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int a=1;
+    do{
+    if(a%2==0&&a%3==0)
+            System.out.println(a);
+    a++;}
+    while(a<=100);    
+    
+    
+    
+    
+    }
+    
+}
